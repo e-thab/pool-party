39fb7acefff7b53c7d2e25a3feefbabca0af50ec
Ethan <1980554+Athen-H@users.noreply.github.com>
2022-08-15 16:54:28 -0400

    Initial commit)

---
+++ scenes/Player1.gd
@@ -0,0 +1,38 @@
+extends Area2D
+
+
+export var speed = 200
+var screen_size
+
+
+func _ready():
+	screen_size = get_viewport_rect().size
+
+
+func _process(delta):
+	# get movement input
+	var velocity = Vector2.ZERO
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("move_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("move_up"):
+		velocity.y -= 1
+	
+	# play animation on move
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		$AnimatedSprite.play("run")
+	else:
+		$AnimatedSprite.play("idle")
+	
+	# flip animation by direction -- this is too quick. should keep flip_h somehow
+	$AnimatedSprite.flip_h = velocity.x < 0
+	
+	# apply velocity to position
+	position += velocity * delta
+	# clamp character into screen bounds
+	position.x = clamp(position.x, 0, screen_size.x)
+	position.y = clamp(position.y, 0, screen_size.y)
